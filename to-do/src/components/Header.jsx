70dfe69f147e7ae1fb961c53ee6cbe59e62193ec
Karol <kypacheco07@misena.edu.co>
2022-08-27 19:27:39 -0500

    Sticky nav

--- to-do/src/components/Header.jsx
+++ to-do/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Button } from "./Button";
 export const Header = () => {
   return (
     <>
-      <nav className="bg-slate-800 sticky w-full flex flex-wrap items-center flex justify-between py-3 bg-gray-100  shadow-lg">
+      <nav className="  sticky top-0 z-50 bg-slate-800  w-full flex flex-wrap items-center flex justify-between py-3 bg-gray-100  shadow-lg">
         <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
           <p className="font-medium leading-tight text-3xl mt-0 mb-2 text-orange-500">
             Kanban App
